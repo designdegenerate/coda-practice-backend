b8970fa58de27230162c3eec5dcddf060a0c30d0
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-18 18:00:42 +0200

    feat/set up database with migrations, models, seeds

---
+++ migrations/20220518150148-create-story.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('storys', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      imageURL: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('storys');
+  }
+};
--- seeders/20200318212758-users.js
+++ seeders/20200318212758-users.js
@@ -4,6 +4,8 @@ const { SALT_ROUNDS } = require("../config/constants");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    //Seed users
     await queryInterface.bulkInsert(
       "users",
       [
@@ -24,9 +26,70 @@ module.exports = {
       ],
       {}
     );
+
+    //seed space
+    await queryInterface.bulkInsert(
+      "spaces",
+      [
+        {
+          title: "SpaceA",
+          description: "spaceADescript",
+          backgroundColor: "",
+          color: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: 1
+        },
+        {
+          title: "SpaceB",
+          description: "spaceBDescript",
+          backgroundColor: "#ff0000",
+          color: "#ffffff",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: 2
+        }
+      ],
+      {}
+    );
+
+    //seed stories
+    await queryInterface.bulkInsert(
+      "storys",
+      [
+        {
+          name: "story 1",
+          content: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
+          imageURL: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          spaceId: 1
+        },
+        {
+          name: "story 2 longer much long ertitl",
+          content: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+          imageURL: "https://i.kym-cdn.com/photos/images/newsfeed/001/207/210/b22.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          spaceId: 1
+        },
+        {
+          name: "story 3",
+          content: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+          imageURL: "https://images.easytechjunkie.com/man-in-black-working-at-computer.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          spaceId: 2
+        },
+      ],
+      {}
+    );
+
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("users", null, {});
+    await queryInterface.bulkDelete("spaces", null, {});
+    await queryInterface.bulkDelete("storys", null, {});
   },
 };
