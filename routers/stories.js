907c0c8edd76d431b5d6d792fe644c6dac8f9a2e
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-20 15:35:41 +0200

    feat/new endpoint for creating stories

--- routers/stories.js
+++ routers/stories.js
@@ -2,6 +2,31 @@ const story = require("../models").story;
 const { Router } = require("express");
 const router = new Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const { spaceId, name, content, imageURL } = req.body;
+
+    if(!spaceId || !name) {
+      return res
+        .status(400)
+        .send({ message: "missing input" });
+    }
+
+    await story.create({
+      name: name,
+      content: content,
+      imageURL: imageURL,
+      spaceId: spaceId
+    })
+
+    res.send(story);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Something went wrong, sorry" });
+  }
+})
+
 router.delete("/:id", async (req, res) => {
   const params = req.params;
   try {
